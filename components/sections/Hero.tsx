5f2c11bc7278359c6da32d6a8f4c278ab0e488d0
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-05 22:04:24 +0100

    Add Link component to resume button

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Link, Typography } from "@mui/material";
 import React from "react";
 import { motion } from "framer-motion";
 import SectionContainer from "../Container/SectionContainer";
@@ -34,9 +34,10 @@ const Hero = () => {
           <Typography
             variant="h1"
             sx={{
-              fontSize: "clamp(5px,8vw,80px)",
+              fontSize: { xs: "clamp(50px,8vw,80px)" },
               fontWeight: 700,
               lineHeight: "1.3",
+              fontFamily: "Abril Fatface",
             }}
           >
             Amir Kazemi
@@ -52,8 +53,9 @@ const Hero = () => {
             variant="h1"
             sx={{
               color: "text.secondary",
-              fontSize: "clamp(4px,7vw,70px)",
+              fontSize: { xs: "clamp(40px,7vw,70px)" },
               fontWeight: 500,
+              fontFamily: "Abril Fatface",
             }}
           >
             I build things for web.
@@ -66,7 +68,12 @@ const Hero = () => {
           transition={{ duration: 0.9, delay: 0.8 }}
         >
           <Typography
-            sx={{ color: "text.secondary", width: "80%", p: "10px 0" }}
+            sx={{
+              color: "text.secondary",
+              width: "80%",
+              p: "10px 0",
+              fontFamily: "Raleway",
+            }}
             variant="h3"
           >
             I'm a UI/UX designer and developer Specializing in building and
@@ -82,18 +89,20 @@ const Hero = () => {
           animate={{ opacity: 1, translateX: 0, translateY: 0 }}
           transition={{ duration: 0.9, delay: 1 }}
         >
-          <Button
-            variant="outlined"
-            sx={{
-              width: { xs: "70vw", sm: "280px" },
-              height: "50px",
-              mt: "10%",
-              display: "inline-block",
-              fontSize: "clamp(19px,5vw,21px)",
-            }}
-          >
-            show resume
-          </Button>
+          <Link href="/resume">
+            <Button
+              variant="outlined"
+              sx={{
+                width: { xs: "70vw", sm: "280px" },
+                height: "50px",
+                mt: "10%",
+                display: "inline-block",
+                fontSize: "clamp(19px,5vw,21px)",
+              }}
+            >
+              show resume
+            </Button>
+          </Link>
         </motion.div>
       </Box>
     </SectionContainer>
