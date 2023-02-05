20b3e349559c64c37330b89d1fd0de2bd11d1aae
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-05 20:59:05 +0100

    Add new font

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,7 +46,7 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Playfair+Display:ital,wght@1,500;1,700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Oswald:wght@200;300;400;500;600;700&family=Playfair+Display:ital,wght@1,500;1,700&display=swap"
           rel="stylesheet"
         />
       </Head>
