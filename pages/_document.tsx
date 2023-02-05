dabe5cb4fc0121440c30f8fd71b428fa225a578d
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-05 23:54:12 +0100

    Remove title from Head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Amir Kazemi Portfolio</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Oswald:wght@200;300;400;500;600;700&family=Playfair+Display:ital,wght@1,500;1,700&display=swap"
           rel="stylesheet"
