00c01445aa6c9d9ead6bb14e84d224ee2e65f5a7
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-05 23:44:28 +0100

    Change Title of page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>Amir Kazemi Portfolio</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Oswald:wght@200;300;400;500;600;700&family=Playfair+Display:ital,wght@1,500;1,700&display=swap"
           rel="stylesheet"
