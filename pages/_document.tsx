dd8c44c50a3fe0951c2bdf14e051d28be9628080
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-05 22:03:29 +0100

    Add Link rel stylesheet to Head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,12 @@ import theme from "../utils/theme";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Oswald:wght@200;300;400;500;600;700&family=Playfair+Display:ital,wght@1,500;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body style={{ backgroundColor: theme.palette.background.paper }}>
         <Main />
         <NextScript />
