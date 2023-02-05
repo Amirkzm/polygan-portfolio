df5a3ee9a03dc63ae5614f56b3426441fc202ced
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-05 16:48:09 +0100

    Add skillItem component for each skills

---
+++ components/SkillItem.tsx
@@ -0,0 +1,32 @@
+import { Box, Paper, Typography } from "@mui/material";
+import Image from "next/image";
+type SkillItemProps = {
+  imageSrc: string;
+  skillName: string;
+};
+
+const SkillItem = (props: SkillItemProps) => {
+  const { imageSrc, skillName } = props;
+
+  return (
+    <Paper
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        width: "150px",
+        padding: "20px 20px",
+        transition: "all 0.3s",
+        "&:hover": {
+          transform: "translateY(-5px)",
+        },
+      }}
+    >
+      <Image src={imageSrc} width="36" height="36" alt="/" />
+      <Typography variant="body1" sx={{ alignSelf: "center" }}>
+        {skillName}
+      </Typography>
+    </Paper>
+  );
+};
+
+export default SkillItem;
