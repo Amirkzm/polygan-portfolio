51293a3d3414c376c615cdad21e69cc634637546
Amir Kazemi <kazemi.amir68@gmail.com>
2023-02-08 22:50:54 +0100

    Remove  redundant log inside LoadingProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <LoadingProvider>
       <ThemeProvider theme={theme}>
         {!loadingCtx.isLoading && <Header />}
+        {/* {console.log(loadingCtx.isLoading)} */}
         <Component {...pageProps} />
         {!loadingCtx.isLoading && <Footer />}
       </ThemeProvider>
